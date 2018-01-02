ddbe87cd29ddd4151917af52e56126a9c9478295
13501233302@139.com <houyafei110>
2018-01-02 15:02:40 +0800

    -未知修改，老版本整理提交

--- blog-utils/src/main/java/com/tommonkey/utils/security/Test.java
+++ blog-utils/src/main/java/com/tommonkey/utils/security/Test.java
@@ -1,12 +1,13 @@
 package com.tommonkey.utils.security;
 
 /**
+ * 数据库用户名密码，加密解决类
  * Created by hyf on 2017/4/17.
  */
 public class Test {
     public static void main(String[] args) throws Exception {
-        System.out.println(K.j("admin1"));//加密
-        System.out.println(K.k("Wc1HLcrn"));//解密
+        System.out.println(K.j("1qaz@WSX"));//加密
+        System.out.println(K.k("OF7VgHjVEko="));//解密
 
     }
 
