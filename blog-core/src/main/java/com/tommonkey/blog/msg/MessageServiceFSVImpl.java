c71604c96656e79e714c2306274f63f3cebd2cb3
13501233302@139.com <houyafei110>
2018-01-02 14:37:59 +0800

    -未知修改，老版本整理提交

--- blog-core/src/main/java/com/tommonkey/blog/msg/MessageServiceFSVImpl.java
+++ blog-core/src/main/java/com/tommonkey/blog/msg/MessageServiceFSVImpl.java
@@ -2,15 +2,14 @@ package com.tommonkey.blog.msg;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.tommonkey.blog.api.msg.IMessageServiceFSV;
+import com.tommonkey.blog.msg.service.interfaces.IMessageServiceSV;
 import com.tommonkey.utils.bp.Constants;
 import com.tommonkey.utils.common.JedisUtils;
 import com.tommonkey.utils.common.JsonUtil;
 import com.tommonkey.utils.response.Response;
-import com.tommonkey.utils.sec.SessionManager;
 import com.tommonkey.utils.sec.entity.MessageInterface;
-import com.tommonkey.utils.sec.entity.UserInfoInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -20,6 +19,8 @@ import java.util.Map;
 @Service
 public class MessageServiceFSVImpl implements IMessageServiceFSV {
 
+    @Autowired
+    private IMessageServiceSV iMessageServiceSV;
     /**
      * 发送消息服务
      * @param param
@@ -32,36 +33,7 @@ public class MessageServiceFSVImpl implements IMessageServiceFSV {
         long result = 0 ;
         Map<String, Object> map = JsonUtil.json2Map(param);
         try {
-            MessageInterface msgInterface = new MessageInterface();
-            String receiveSessionId = (String) map.get("receiveSessionId");
-            Integer msgType = Integer.parseInt(map.get("msgType").toString());
-            Integer sendType = Integer.parseInt(map.get("sendType").toString());
-            String msgTitle = (String) map.get("msgTitle");
-            Integer msgShowTime = Integer.parseInt(map.get("msgShowTime").toString());
-            String msgContent = (String) map.get("msgContent");
-            String msgShowType = (String) map.get("msgShowType");
-            String showPosition = (String) map.get("showPosition");
-
-            UserInfoInterface sendUser = SessionManager.getUser();
-            UserInfoInterface receUser = SessionManager.getUser(receiveSessionId);
-            if(sendUser == null || receUser == null){
-                throw new Exception("连接中断，请重新登陆后再试");
-            }
-            msgInterface.setSendUser(sendUser);
-            msgInterface.setReceiveUser(receUser);
-            msgInterface.setSendDate(new Date());
-            msgInterface.setEffTime(60*60*24);//暂定24小时
-            msgInterface.setMsgType(msgType);
-            msgInterface.setSendType(sendType);
-            msgInterface.setMsgState(0);
-            msgInterface.setMsgTitle(msgTitle);
-            msgInterface.setMsgContent(msgContent);
-            msgInterface.setMsgShowTime(msgShowTime);
-            msgInterface.setMsgShowType(msgShowType);
-            msgInterface.setShowPosition(showPosition);
-
-            //将消息放入队列
-            result = JedisUtils.listObjectAdd(Constants.Sys.MSG_LIST,msgInterface,0);
+            result = iMessageServiceSV.sendMsg(map);
             if(result == 0){
                 throw new Exception("消息发送失败");
             }
